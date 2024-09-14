d8e2e1cd3e226619b7cacbf416dc73201737fa04
anasaqeeel <122775288+anasaqeeel@users.noreply.github.com>
2024-09-14 21:12:20 +0500

    Add: React Modules

--- bundler/webpack.dev.js
+++ bundler/webpack.dev.js
@@ -1,44 +1,16 @@
-const path = require('path');
 const { merge } = require('webpack-merge');
-const commonConfiguration = require('./webpack.common.js');
-const portFinderSync = require('portfinder-sync');
+const common = require('./webpack.common.js');
+const path = require('path');
 
-const infoColor = (_message) => {
-    return `\u001b[1m\u001b[34m${_message}\u001b[39m\u001b[22m`;
-};
-
-module.exports = merge(commonConfiguration, {
-    stats: 'errors-warnings',
-    mode: 'development',
-    infrastructureLogging: {
-        level: 'warn',
+module.exports = merge(common, {
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '../dist'),
     },
-    devServer: {
-        host: '0.0.0.0', // Bind to all network interfaces
-        port: portFinderSync.getPort(8080), // Automatically find an available port
-        open: true, // Open the browser on server start
-        https: false, // Use HTTP
-        allowedHosts: 'all', // Allow all hosts
-        hot: true, // Enable hot module replacement
-        watchFiles: ['src/**', 'static/**'], // Watch these files for changes
-        static: {
-            watch: true, // Watch static files for changes
-            directory: path.join(__dirname, '../static') // Static file directory
-        },
-        client: {
-            logging: 'none', // No client-side logging
-            overlay: true, // Show overlay for build errors
-            progress: true // Show build progress
-        },
-        setupMiddlewares: (middlewares, devServer) => {
-            console.log('------------------------------------------------------------');
-            console.log(`Host: ${devServer.options.host}`);
-            const port = devServer.options.port;
-            const https = devServer.options.https ? 's' : '';
-            const domain1 = `http${https}://${devServer.options.host}:${port}`;
-            const domain2 = `http${https}://localhost:${port}`;
-            console.log(`Project running at:\n  - ${infoColor(domain1)}\n  - ${infoColor(domain2)}`);
-            return middlewares;
-        }
-    }
+    compress: true,
+    port: 9000,
+    historyApiFallback: true, // For React Router
+  },
+  devtool: 'inline-source-map',
 });
---
+++ src/pages/EasyRoom.js
@@ -0,0 +1,25 @@
+// src/pages/EasyRoom.js
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import EasyRoomScene from '../components/EasyRoomScene';
+import { OrbitControls } from '@react-three/drei';
+import './EasyRoom.css';
+
+const EasyRoom = () => (
+  <div className="canvas-container">
+    {/* This div ensures the Canvas fills the viewport */}
+    <Canvas
+      camera={{ position: [-2, 1.995, 2.9], fov: 45, near: 0.1, far: 1000 }}
+      className="canvas"
+    >
+      <ambientLight intensity={0.5} color={0xffffff} />
+      <directionalLight intensity={1} position={[10, 10, 10]} color={0xffffff} />
+      <Suspense fallback={null}>
+        <EasyRoomScene />
+      </Suspense>
+      <OrbitControls enableDamping={true} />
+    </Canvas>
+  </div>
+);
+
+export default EasyRoom;
